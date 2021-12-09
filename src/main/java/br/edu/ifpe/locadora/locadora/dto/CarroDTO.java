cabfae26f39c1f66b38fc8b670224501ae937ded
JJunio <junio.araujo01@yahoo.com>
2021-12-09 00:21:58 -0300

    Atualizando a classe CarroDTO

--- src/main/java/br/edu/ifpe/locadora/locadora/dto/CarroDTO.java
+++ src/main/java/br/edu/ifpe/locadora/locadora/dto/CarroDTO.java
@@ -15,12 +15,14 @@ public class CarroDTO {
 	private String marca;
 	private String cor;
 	private String ano;
+	private boolean disponivel;
 
 	public CarroDTO(Carro carro) {
 //		this.id = carro.getId();
 //		this.ano = carro.getAno();
 //		this.marca = carro.getMarca();
 //		this.cor = carro.getCor();
+		// this.disponivel = carro.getDisponivel();
 	}
 
 	public long getId() {
@@ -37,6 +39,10 @@ public class CarroDTO {
 
 	public String getAno() {
 		return ano;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
 	}
 
 	public static List<CarroDTO> converterListaCarroDTO(List<Carro> carros) {
